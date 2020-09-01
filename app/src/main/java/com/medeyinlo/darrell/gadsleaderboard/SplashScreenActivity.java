be8cd4edb051bb2a1f10ff83b432690e941d27b4
Darrell <mediumgeek@gmail.com>
2020-09-01 21:06:38 +0100

    Finished toolbar in MainActivity; added plain background and splash screen files; created splash screen activity

---
+++ app/src/main/java/com/medeyinlo/darrell/gadsleaderboard/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.medeyinlo.darrell.gadsleaderboard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
